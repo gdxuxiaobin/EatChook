eb003744a2b773ef8d8c5336bd0e6859ab6efe16
xuxiaobin <1136372796@qq.com>
2023-12-08 12:16:30 +0800

    20231208

--- temp/declarations/jsb.d.ts
+++ temp/declarations/jsb.d.ts
@@ -1,1 +1,1 @@
-/// <reference path="D:\Program Files\CocosEditors\Creator\3.6.2\resources\resources\3d\engine\@types\jsb.d.ts"/>
+/// <reference path="D:\Program Files\CocosEditors\Creator\3.7.2\resources\resources\3d\engine\@types\jsb.d.ts"/>
